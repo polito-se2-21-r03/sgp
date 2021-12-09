41cf6b0ce83be9f7c3fa12253a14f09b1f36d150
Simone <simone.alberto@icloud.com>
2021-12-09 17:14:16 +0100

    Added description

--- client/src/pages/Clients/ClientsProductAll/AddedProductRow.tsx
+++ client/src/pages/Clients/ClientsProductAll/AddedProductRow.tsx
@@ -4,8 +4,16 @@ import {
   TextField,
   Stack,
   TextStyle,
-  Button
+  Button,
+  Collapsible,
+  TextContainer,
+  Link
 } from '@shopify/polaris';
+
+import {
+  ChevronDownMinor,
+  ChevronUpMinor
+} from '@shopify/polaris-icons';
 
 export function AddedProductRow({ item, addProduct }: any) {
   const { price } = item;
@@ -21,6 +29,9 @@ export function AddedProductRow({ item, addProduct }: any) {
     addProduct(item, value);
   }, [value]);
 
+  const [open, setOpen] = useState(false);
+  const handleToggle = useCallback(() => setOpen((open) => !open), []);
+
   return (
     <Stack vertical>
       <Stack.Item>
@@ -32,7 +43,33 @@ export function AddedProductRow({ item, addProduct }: any) {
       </Stack.Item>
       <Stack.Item>
         <TextStyle variation="strong">{item.name}</TextStyle>
-        <p><TextStyle variation="subdued">Farmer: {item.farmer}</TextStyle></p>
+        <p>
+          <Stack distribution="equalSpacing">
+            <TextStyle variation="subdued">Farmer: {item.farmer}</TextStyle>
+            <Button
+              plain
+              onClick={handleToggle}
+              ariaExpanded={open}
+              ariaControls="basic-collapsible"
+              icon={open ? ChevronUpMinor : ChevronDownMinor}
+            />
+          </Stack>
+        </p>
+        <Collapsible
+          open={open}
+          id="basic-collapsible"
+          transition={{ duration: '500ms', timingFunction: 'ease-in-out' }}
+          expandOnPrint
+        >
+          <TextContainer>
+            <p>
+              Your mailing list lets you contact customers or visitors who
+              have shown an interest in your store. Reach out to them with
+              exclusive offers or updates about your products.
+              {/* {item.description} */}
+            </p>
+          </TextContainer>
+        </Collapsible>
       </Stack.Item>
       <Stack.Item>
         <Stack distribution="equalSpacing" alignment="center">
